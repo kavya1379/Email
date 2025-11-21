f37e8be0092553de59bd35214f472c8374a890f6
kavya1379 <horkerikavya@gmail.com>
2025-11-21 10:41:44 +0530

    discription

--- Email.py
+++ Email.py
@@ -2,7 +2,8 @@ import sys
 
 if len(sys.argv) == 1:
   email=sys.argv[1:]
+  print("User provided input values:")
 else:
-   email = "horkerikavya@gmail.com"
-   print(f"Email address:{email}")
+   email="horkerikavya@gmail.com"
+   print("Email address:",email)
 
