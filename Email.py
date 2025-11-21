62e700af93363cff4e38dc79959194c20a1e6fdf
kavya1379 <horkerikavya@gmail.com>
2025-11-21 10:50:48 +0530

    discription

--- Email.py
+++ Email.py
@@ -1,9 +1,10 @@
+
 import sys
 
-if len(sys.argv) == 1:
-  email=sys.argv[1:]
-  print("User provided input values:")
+if len(sys.argv) > 1:
+    email = sys.argv[1:] 
+    print("User provided input values:")
 else:
-   email="horkerikavya@gmail.com"
-   print("Email address:",email)
-
+    print("No command-line input provided. Using default values:")
+    email="horkerikavya@gmail.com"
+    print("Email address:",email)
